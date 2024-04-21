c1fd1e88bd5af8201dab7f49249765e4ab30c026
victorc60 <105111283+victorc60@users.noreply.github.com>
2024-04-21 22:02:22 +0300

    to the fisrt commit

--- src/components/Shoplist/Shoplist.jsx
+++ src/components/Shoplist/Shoplist.jsx
@@ -1,54 +1,46 @@
 import React, { useState, useEffect } from "react";
-import io from "socket.io-client";
 import styles from "./Shoplist.module.css";
-
-// Подключение к серверу Socket.IO
-const socket = io("https://shoppig-victorc60s-projects.vercel.app/"); // Укажите URL вашего сервера, если он различается
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHandPointRight } from "@fortawesome/free-solid-svg-icons";
 
 function Shoplist() {
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState([]);
 
+  function loadCart() {
+    const cartData = localStorage.getItem("cart");
+    if (cartData) {
+      setItems(JSON.parse(cartData));
+    }
+  }
+
   useEffect(() => {
-    // Получение начального списка элементов при подключении
-    socket.on("initialItems", (initialItems) => {
-      setItems(initialItems);
-    });
-
-    // Подписка на события добавления и удаления элементов
-    socket.on("itemAdded", (item) => {
-      setItems((prevItems) => [item, ...prevItems]);
-    });
-
-    socket.on("itemRemoved", (id) => {
-      setItems((prevItems) => prevItems.filter((item) => item.id !== id));
-    });
-
-    // Очистка подписок при размонтировании компонента
-    return () => {
-      socket.off("initialItems");
-      socket.off("itemAdded");
-      socket.off("itemRemoved");
-    };
+    loadCart();
   }, []);
 
   function addItem() {
     const item = {
-      id: Date.now(), // Генерация уникального ID для каждого элемента
+      id: Date.now(),
       value: newItem,
     };
-    socket.emit("addItem", item); // Отправка нового элемента на сервер
-    setNewItem(""); // Очистка поля ввода
+    setItems((prevItems) => [item, ...prevItems]);
+    const cartData = JSON.parse(localStorage.getItem("cart")) || [];
+    cartData.push(item);
+    localStorage.setItem("cart", JSON.stringify(cartData));
+    setNewItem("");
   }
 
   function removeItem(id) {
-    socket.emit("removeItem", id); // Отправка ID удаляемого элемента на сервер
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+    const cartData = JSON.parse(localStorage.getItem("cart")) || [];
+    const updatedCart = cartData.filter((item) => item.id !== id);
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
   }
 
   return (
     <>
       <div className={styles.container}>
-        <h2>Add some items in cart</h2>
+        <h2>Add some items in cart </h2>
 
         <div className={styles.input__container}>
           <input
